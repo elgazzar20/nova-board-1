c19334b496826965972f0c1730019904fea9d2f9
elgazzar20 <mohamedelgazzar700@gmail.com>
2026-06-28 01:19:52 +0300

    1

--- test-img.js
+++
@@ -1,5 +0,0 @@
-import https from 'https';
-https.get('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/World_map_blank_without_borders.svg/1024px-World_map_blank_without_borders.svg.png', { headers: { 'User-Agent': 'Mozilla/5.0' } }, (res) => {
-  console.log(res.statusCode);
-  console.log(res.headers);
-});
